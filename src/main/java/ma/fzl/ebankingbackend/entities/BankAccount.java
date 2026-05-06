b4d5f0b51cbbd4a2f2c4727cba34eafced9c947e
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-06 15:59:07 +0100

    Add BankAccount entity

---
+++ src/main/java/ma/fzl/ebankingbackend/entities/BankAccount.java
@@ -0,0 +1,25 @@
+package ma.fzl.ebankingbackend.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.fzl.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperations;
+}
