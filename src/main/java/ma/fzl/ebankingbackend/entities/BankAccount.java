cb3ccbfa31c536c40d942f0a4d7482c99b9ff99a
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-06 16:23:05 +0100

    Modify the BankAccount, CurrentAccount and the SavingAccount to respect the Single Table strategy

--- src/main/java/ma/fzl/ebankingbackend/entities/BankAccount.java
+++ src/main/java/ma/fzl/ebankingbackend/entities/BankAccount.java
@@ -11,6 +11,8 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE" , length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
     @Id
