73465cc10349a5e217639cf7f7c1c6f6432ed3ad
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 21:27:21 +0100

    Add implementation of Bank Account Mapper

---
+++ src/main/java/ma/fzl/ebankingbackend/mappers/BankAccountMapperImpl.java
@@ -0,0 +1,22 @@
+package ma.fzl.ebankingbackend.mappers;
+
+import ma.fzl.ebankingbackend.dtos.CustomerDTO;
+import ma.fzl.ebankingbackend.entities.Customer;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+//MapStruct
+@Service
+public class BankAccountMapperImpl {
+    public CustomerDTO fromCustomer (Customer customer){
+        CustomerDTO customerDTO  =new CustomerDTO();
+        BeanUtils.copyProperties(customer, customerDTO);
+        return customerDTO;
+    }
+    public Customer fromCustomerDTO (CustomerDTO customerDTO){
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDTO,customer);
+        return customer;
+    }
+}
+
