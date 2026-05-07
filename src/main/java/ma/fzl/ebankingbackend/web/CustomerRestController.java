8df4f749590c6401ec3243d628c2d498b265c83f
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 19:58:43 +0100

    Add a Customer Controller

---
+++ src/main/java/ma/fzl/ebankingbackend/web/CustomerRestController.java
@@ -0,0 +1,22 @@
+package ma.fzl.ebankingbackend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.fzl.ebankingbackend.entities.Customer;
+import ma.fzl.ebankingbackend.services.BankAccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+    @GetMapping("/customers")
+    public List<Customer> customers(){
+        return bankAccountService.listCustomer();
+    }
+}
