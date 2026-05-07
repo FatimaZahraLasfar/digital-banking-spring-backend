7a942dad8645517827e3e9e38c94b66654d86fd2
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 20:00:43 +0100

    Add a Customer DTO

---
+++ src/main/java/ma/fzl/ebankingbackend/mappers/CustomerDTO.java
@@ -0,0 +1,17 @@
+package ma.fzl.ebankingbackend.mappers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.fzl.ebankingbackend.entities.BankAccount;
+
+import java.util.List;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
