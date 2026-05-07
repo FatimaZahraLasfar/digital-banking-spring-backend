50a6fb7b10e269d3ddf4751f9dbc0fc31caf4d4f
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 00:04:05 +0100

    Add Saving bank account DTO

--- src/main/java/ma/fzl/ebankingbackend/dtos/SavingBankAccountDTO.java
+++ src/main/java/ma/fzl/ebankingbackend/dtos/SavingBankAccountDTO.java
@@ -1,16 +1,7 @@
 package ma.fzl.ebankingbackend.dtos;
-
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import ma.fzl.ebankingbackend.entities.AccountOperation;
-import ma.fzl.ebankingbackend.entities.Customer;
 import ma.fzl.ebankingbackend.enums.AccountStatus;
-
 import java.util.Date;
-import java.util.List;
 
 @Data
 public  class SavingBankAccountDTO {
