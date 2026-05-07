7fb2f0a1dce90c32b25d9e959bbfbf983f4f20c5
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 23:44:25 +0100

    Add the Customer Rest Controller

--- src/main/java/ma/fzl/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/ma/fzl/ebankingbackend/web/CustomerRestController.java
@@ -34,7 +34,7 @@ public class CustomerRestController {
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     }
-    @DeleteMapping("/customers/'{id}")
+    @DeleteMapping("/customers/{id}")
     public void deleteCustomer(@PathVariable Long id){
         bankAccountService.deleteCustomer(id);
     }
