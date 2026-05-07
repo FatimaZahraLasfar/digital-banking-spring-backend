a0129a2b99c8cf7640f641c82e95436783985772
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 19:38:31 +0100

    Add bankAccountsList to the Interface BankAccountService

--- src/main/java/ma/fzl/ebankingbackend/services/BankAccountService.java
+++ src/main/java/ma/fzl/ebankingbackend/services/BankAccountService.java
@@ -14,9 +14,10 @@ public interface BankAccountService {
     Customer saveCustomer(Customer customer);
     CurrentAccount saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
     SavingAccount saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
-    List<Customer> ListCustomer();
+    List<Customer> listCustomer();
     BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException;
     void debit(String accountId, double amount, String description ) throws BankAccountNotFoundException, BalanceNotSufficentException;
     void credit(String accountId, double amount, String description ) throws BankAccountNotFoundException;
     void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficentException;
+    List<BankAccount> bankAccountsList();
 }
