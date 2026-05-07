20d36032c520a3d77d1e14cff61a6bd8a50e7548
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 00:40:49 +0100

    Add Bank Account Rest Controller

--- src/main/java/ma/fzl/ebankingbackend/web/BankAccountRestController.java
+++ src/main/java/ma/fzl/ebankingbackend/web/BankAccountRestController.java
@@ -1,8 +1,13 @@
 package ma.fzl.ebankingbackend.web;
 
 import lombok.AllArgsConstructor;
+import ma.fzl.ebankingbackend.dtos.BankAccountDTO;
+import ma.fzl.ebankingbackend.exceptions.BankAccountNotFoundException;
 import ma.fzl.ebankingbackend.services.BankAccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class BankAccountRestController {
