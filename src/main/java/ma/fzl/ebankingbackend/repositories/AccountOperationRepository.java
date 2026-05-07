c224d0d9910bb8d935d56b846a55a034e816908c
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 15:33:17 +0100

    Add AccountOperationRepository Repository extending from the JpaRepository

---
+++ src/main/java/ma/fzl/ebankingbackend/repositories/AccountOperationRepository.java
@@ -0,0 +1,10 @@
+package ma.fzl.ebankingbackend.repositories;
+
+import ma.fzl.ebankingbackend.entities.AccountOperation;
+import ma.fzl.ebankingbackend.entities.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountOperationRepository extends JpaRepository <AccountOperation, Long> {
+}
