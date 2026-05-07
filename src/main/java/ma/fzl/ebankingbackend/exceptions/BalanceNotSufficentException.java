16d9ff2abc998ff1e80cd153c7026669d1f0263c
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-07 19:16:49 +0100

    Define the exceptions needed

---
+++ src/main/java/ma/fzl/ebankingbackend/exceptions/BalanceNotSufficentException.java
@@ -0,0 +1,7 @@
+package ma.fzl.ebankingbackend.exceptions;
+
+public class BalanceNotSufficentException extends Exception {
+    public BalanceNotSufficentException(String message) {
+        super(message);
+    }
+}
