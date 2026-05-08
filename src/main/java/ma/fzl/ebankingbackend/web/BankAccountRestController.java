fa78c7f4ddc230010bb0df6f0eb3d80d48d77d4e
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 01:41:21 +0100

    Add Bank Account Rest Controller

--- src/main/java/ma/fzl/ebankingbackend/web/BankAccountRestController.java
+++ src/main/java/ma/fzl/ebankingbackend/web/BankAccountRestController.java
@@ -1,11 +1,14 @@
 package ma.fzl.ebankingbackend.web;
 
 import lombok.AllArgsConstructor;
+import ma.fzl.ebankingbackend.dtos.AccountHistoryDTO;
+import ma.fzl.ebankingbackend.dtos.AccountOperationDTO;
 import ma.fzl.ebankingbackend.dtos.BankAccountDTO;
 import ma.fzl.ebankingbackend.exceptions.BankAccountNotFoundException;
 import ma.fzl.ebankingbackend.services.BankAccountService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -28,8 +31,17 @@ public class BankAccountRestController {
     }
 
     @GetMapping("/accounts/{accountId}/operations")
-    public List<AccountOperationDTO> getHistory (String accountId){
+    public List<AccountOperationDTO> getHistory (@PathVariable String accountId){
         return bankAccountService.accountHistory(accountId);
+    }
+
+    @GetMapping("/accounts/{accountId}/pageOperations")
+    public AccountHistoryDTO getAccountHistory (
+            @PathVariable String accountId,
+            @RequestParam(name="page" , defaultValue = "0") int page,
+            @RequestParam(name="size", defaultValue = "5") int size)
+            throws BankAccountNotFoundException{
+        return bankAccountService.getAccountHistory(accountId,page,size);
     }
 }
 
