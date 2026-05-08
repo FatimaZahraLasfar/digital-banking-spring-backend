62c8d0fb5aff56bd2b85acba65b826e47e5356e0
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 16:05:14 +0100

    Use Query annotation to search

--- src/main/java/ma/fzl/ebankingbackend/repositories/CustomerRepository.java
+++ src/main/java/ma/fzl/ebankingbackend/repositories/CustomerRepository.java
@@ -2,11 +2,14 @@ package ma.fzl.ebankingbackend.repositories;
 
 import ma.fzl.ebankingbackend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository <Customer , Long> {
-    List<Customer> findByNameContains(String keyword);
+    @Query("select c from Customer c where c.name like kw")
+    List<Customer> searchCustomer(@Param("kw") String keyword);
 }
