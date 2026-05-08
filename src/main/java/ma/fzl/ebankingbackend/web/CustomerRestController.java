a5ed678c602dbe72a27748207a4f7570e70403e8
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 21:32:54 +0100

    LC

--- src/main/java/ma/fzl/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/ma/fzl/ebankingbackend/web/CustomerRestController.java
@@ -23,7 +23,7 @@ public class CustomerRestController {
 
     @GetMapping("/customers/search")
     public List<CustomerDTO> searchCustomers(@RequestParam(name="keyword",defaultValue = "") String keyword){
-        return bankAccountService.searchCustomers(keyword);
+        return bankAccountService.searchCustomers("%"+keyword+"%");
     }
 
 
