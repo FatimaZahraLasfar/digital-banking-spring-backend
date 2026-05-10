30f867a1017685d4971021fe32a19550f2efd418
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-10 02:38:10 +0100

    add debit, credit, transfer endpoints and @PreAuthorize role guards

--- src/main/java/ma/fzl/ebankingbackend/web/BankAccountRestController.java
+++ src/main/java/ma/fzl/ebankingbackend/web/BankAccountRestController.java
@@ -1,46 +1,76 @@
 package ma.fzl.ebankingbackend.web;
 
 import lombok.AllArgsConstructor;
-import ma.fzl.ebankingbackend.dtos.AccountHistoryDTO;
-import ma.fzl.ebankingbackend.dtos.AccountOperationDTO;
-import ma.fzl.ebankingbackend.dtos.BankAccountDTO;
+import ma.fzl.ebankingbackend.dtos.*;
+import ma.fzl.ebankingbackend.exceptions.BalanceNotSufficentException;
 import ma.fzl.ebankingbackend.exceptions.BankAccountNotFoundException;
 import ma.fzl.ebankingbackend.services.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin("*")
+@AllArgsConstructor
 public class BankAccountRestController {
-    private BankAccountService bankAccountService;
 
-    public BankAccountRestController(BankAccountService bankAccountService) {
-        this.bankAccountService = bankAccountService;
-    }
+    private final BankAccountService bankAccountService;
 
     @GetMapping("/accounts/{accountId}")
-    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+    @PreAuthorize("hasRole('USER')")
+    public BankAccountDTO getBankAccount(@PathVariable String accountId)
+            throws BankAccountNotFoundException {
         return bankAccountService.getBankAccount(accountId);
     }
 
     @GetMapping("/accounts")
-    public List<BankAccountDTO> listAccounts(){
+    @PreAuthorize("hasRole('USER')")
+    public List<BankAccountDTO> listAccounts() {
         return bankAccountService.bankAccountsList();
     }
 
     @GetMapping("/accounts/{accountId}/operations")
-    public List<AccountOperationDTO> getHistory (@PathVariable String accountId){
+    @PreAuthorize("hasRole('USER')")
+    public List<AccountOperationDTO> getHistory(@PathVariable String accountId) {
         return bankAccountService.accountHistory(accountId);
     }
 
     @GetMapping("/accounts/{accountId}/pageOperations")
-    public AccountHistoryDTO getAccountHistory (
+    @PreAuthorize("hasRole('USER')")
+    public AccountHistoryDTO getAccountHistory(
             @PathVariable String accountId,
-            @RequestParam(name="page" , defaultValue = "0") int page,
-            @RequestParam(name="size", defaultValue = "5") int size)
-            throws BankAccountNotFoundException{
-        return bankAccountService.getAccountHistory(accountId,page,size);
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "5") int size)
+            throws BankAccountNotFoundException {
+        return bankAccountService.getAccountHistory(accountId, page, size);
+    }
+
+    @PostMapping("/accounts/debit")
+    @PreAuthorize("hasRole('USER')")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO)
+            throws BankAccountNotFoundException, BalanceNotSufficentException {
+        bankAccountService.debit(debitDTO.getAccountId(), debitDTO.getAmount(), debitDTO.getDescription());
+        return debitDTO;
+    }
+
+    @PostMapping("/accounts/credit")
+    @PreAuthorize("hasRole('USER')")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO)
+            throws BankAccountNotFoundException {
+        bankAccountService.credit(creditDTO.getAccountId(), creditDTO.getAmount(), creditDTO.getDescription());
+        return creditDTO;
+    }
+
+    @PostMapping("/accounts/transfer")
+    @PreAuthorize("hasRole('USER')")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO)
+            throws BankAccountNotFoundException, BalanceNotSufficentException {
+        bankAccountService.transfer(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount()
+        );
     }
 }
 
