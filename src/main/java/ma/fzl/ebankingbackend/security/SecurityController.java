4e6650c5e54dda6411232ebb05d59e07685e96ef
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-10 02:38:53 +0100

    LC

--- src/main/java/ma/fzl/ebankingbackend/security/SecurityController.java
+++ src/main/java/ma/fzl/ebankingbackend/security/SecurityController.java
@@ -1,16 +1,29 @@
 package ma.fzl.ebankingbackend.security;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class SecurityController {
-    @GetMapping(".profile")
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @GetMapping("/profile")
     public Authentication authentication(Authentication authentication){
         return authentication;
+
+    }
+
+    @PostMapping("/login")
+    public Map<String,String> login(String username, String password){
 
     }
 }
